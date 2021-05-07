76ed8cf2193ec4788e22d24e28eb417cd446d17f
Harr1s96 <hanleyharris96@gmail.com>
2021-05-07 15:23:13 +0100

    added eslint config file

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(_req, res, _next) {
   res.send('respond with a resource');
 });
 
