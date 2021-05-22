06b351b46a16a02df2a5a020d9d39847bdce0afa
Harr1s96 <hanleyharris96@gmail.com>
2021-05-22 22:30:51 +0100

    added cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
 
 require('dotenv').config();
 
 const app = express();
 const { PORT } = process.env;
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
