8849a9104b5b1dd23d60d6a0541c561b4a3bdc59
Harr1s96 <hanleyharris96@gmail.com>
2021-05-22 20:33:53 +0100

    added eslintrc file back in

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 require('dotenv').config();
@@ -10,7 +9,6 @@ const { PORT } = process.env;
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 app.use('/api', require('./routes/form'));
 app.use('/api', require('./routes/ddpo'));
