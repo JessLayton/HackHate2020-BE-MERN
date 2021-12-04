e0055d79dab3be9015ccd71327e21d761d27eb6d
lukecottenham <luke.cottenham@hotmail.co.uk>
2021-12-04 14:21:04 +0000

    refactored and console logs removed

--- routes/dash.js
+++ routes/dash.js
@@ -1,13 +1,15 @@
 const router = require('express').Router();
 const axios = require('axios');
 
-const { PYTHON_URL } = process.env;
-
+const sortAndGroupByQuarter = require('../utilities/sortAndGroupByQuarter');
 const Form = require('../models/formModel');
+
+const { PYTHON_URL } = process.env;
 
 router.get('/reportingDetails', (_req, res) => {
   Form.find({}, ['quarter', 'year', 'reportingDetails'])
     .then((result) => {
+      console.log(result);
       axios.post(`${PYTHON_URL}/reportingNumbers`, result)
         .then((response) => {
           // extract data from response object
@@ -26,15 +28,8 @@ router.get('/reportingDetails', (_req, res) => {
 router.get('/reasons', (_req, res) => {
   Form.find({}, ['unreportedCases', 'quarter', 'year'])
     .then((result) => {
-      axios.post(`${PYTHON_URL}/unreportedCases`, result)
-        .then((response) => {
-          // extract data from response object
-          res.status(200).json(response.data);
-        })
-        .catch((err) => {
-          res.status(500).send(err);
-          console.error(err);
-        });
+      const sortedAndGrouped = sortAndGroupByQuarter(result.map(({ _doc }) => _doc));
+      res.status(200).json(sortedAndGrouped);
     })
     .catch((err) => {
       res.status(500).send(err);
--- utilities/sortAndGroupByQuarter.js
+++ utilities/sortAndGroupByQuarter.js
@@ -1,52 +1,52 @@
 const _ = require('lodash');
 
-const data = [
-  {
-    quarter: 1,
-    year: 2020,
-    value: {
-      test: 2,
-      another: [2, 3, 4],
-    },
-    another: [2, 3, 4],
-  },
-  {
-    quarter: 1,
-    year: 2019,
-    value: {
-      test: 2,
-      another: [2, 3, 4],
-    },
-    another: [2, 3, 4],
-  },
-  {
-    quarter: 1,
-    year: 2019,
-    value: {
-      test: 2,
-      another: [2, 3, 4],
-    },
-    another: [2, 3, 4],
-  },
-  {
-    quarter: 2,
-    year: 2019,
-    value: {
-      test: 2,
-      another: [2, 3, 4],
-    },
-    another: [2, 3, 4],
-  },
-  {
-    quarter: 3,
-    year: 2018,
-    value: {
-      test: 2,
-      another: [2, 3, 4],
-    },
-    another: [2, 3, 4],
-  },
-];
+// const data = [
+//   {
+//     quarter: 1,
+//     year: 2020,
+//     value: {
+//       test: 2,
+//       another: [2, 3, 4],
+//     },
+//     another: [2, 3, 4],
+//   },
+//   {
+//     quarter: 1,
+//     year: 2019,
+//     value: {
+//       test: 2,
+//       another: [2, 3, 4],
+//     },
+//     another: [2, 3, 4],
+//   },
+//   {
+//     quarter: 1,
+//     year: 2019,
+//     value: {
+//       test: 2,
+//       another: [2, 3, 4],
+//     },
+//     another: [2, 3, 4],
+//   },
+//   {
+//     quarter: 2,
+//     year: 2019,
+//     value: {
+//       test: 2,
+//       another: [2, 3, 4],
+//     },
+//     another: [2, 3, 4],
+//   },
+//   {
+//     quarter: 3,
+//     year: 2018,
+//     value: {
+//       test: 2,
+//       another: [2, 3, 4],
+//     },
+//     another: [2, 3, 4],
+//   },
+// ];
 
 const sumObjects = (obj1, obj2) => (
   _.mergeWith(obj1, obj2, (objValue, srcValue) => {
@@ -57,20 +57,21 @@ const sumObjects = (obj1, obj2) => (
   })
 );
 
-let result = _.groupBy(data, ({ year, quarter }) => `${quarter} ${year}`);
-result = Object.entries(result).map(([period, values]) => [period.split(' '), values]);
-result = _.sortBy(result, ['0.1', '0.0']); // 0.1 is the year and 0.0 is the quarter, so sorts by year and then quarter
-result = result.map(([[quarter, year], values]) => {
-  console.log(values);
-  const summedTotals = values.reduce(
-    (previous, {
-      quarter: _quarter, year: _year, _id, ...rest
-    }) => (
-      sumObjects(rest, previous)
-    ), {},
-  );
-  console.log(summedTotals);
-  return { [`Q${quarter} ${year}`]: summedTotals };
-});
+const sortAndGroupByQuarter = (data) => {
+  let result = _.groupBy(data, ({ year, quarter }) => `${quarter} ${year}`);
+  result = Object.entries(result).map(([period, values]) => [period.split(' '), values]);
+  result = _.sortBy(result, ['0.1', '0.0']); // 0.1 is the year and 0.0 is the quarter, so sorts by year and then quarter
+  result = result.map(([[quarter, year], values]) => {
+    const summedTotals = values.reduce(
+      (previous, {
+        quarter: _quarter, year: _year, _id, ...rest
+      }) => (
+        sumObjects(rest, previous)
+      ), {},
+    );
+    return { [`Q${quarter} ${year}`]: summedTotals };
+  });
+  return result;
+};
 
-module.exports = result;
+module.exports = sortAndGroupByQuarter;
